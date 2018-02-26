bae7640d97aaedb2acbaf8614a2556b2cab7923e
John P Considine <jconsidi@nd.edu>
2018-02-25 19:31:42 -0600

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 /**
 * @author Jack Considine <jackconsidine3@gmail.com>
-* @package 
+* @package
 * 2018-02-25
 */
+
+var app = require("./main");
+
+
+app.listen(8000, function () {
+  console.log('Listenings')
+})
